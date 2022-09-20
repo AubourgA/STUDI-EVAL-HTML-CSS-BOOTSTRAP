5b21b680851e9d7c5ab08d09f65c8d7e6221d1ff
AubourgA <adrien.aubourg@hotmail.fr>
2022-09-20 18:39:13 +0200

    show menu with burger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,15 @@ window.addEventListener('DOMContentLoaded', ()=> {
     const navBar = document.querySelector('nav');
     const toggleBtn = document.querySelector('.nav__toggle');
     
+    /*show menu*/ 
+    const overlay = document.querySelector('.overlay');
+    const menu = document.querySelector('.nav__list');
+
+    
     toggleBtn.addEventListener('click', ()=> {
             navBar.classList.toggle('active');
+            overlay.classList.toggle('show');
+           menu.classList.toggle('show');
     })
 
 
